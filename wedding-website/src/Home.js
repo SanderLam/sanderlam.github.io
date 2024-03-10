cad043822532ac69486b46d8058ef91172f03518
Sander Lam <sander.lam@teampicnic.com>
2024-03-10 21:31:41 +0100

    Base website

---
+++ wedding-website/src/Home.js
@@ -0,0 +1,18 @@
+// src/Home.js
+import React from "react";
+import RSVPForm from "./RSVPForm";
+
+function Home() {
+  return (
+    <div>
+      <h1>Our Wedding</h1>
+      <p>
+        Welcome to our wedding website! Join us as we celebrate this special
+        day.
+      </p>
+      <RSVPForm />
+    </div>
+  );
+}
+
+export default Home;
